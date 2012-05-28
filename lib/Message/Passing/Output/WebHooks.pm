4e829024e4a4cd06702d06b8de9dc1ad1c6c601c
Tomas Doran <bobtfish@bobtfish.net>
2012-05-28 09:40:19 +0100

    Fix docs

--- lib/Message/Passing/Output/WebHooks.pm
+++ lib/Message/Passing/Output/WebHooks.pm
@@ -102,11 +102,11 @@ sub consume {
 
 =head1 NAME
 
-Message::Passing::Output::WebHooks - call 'WebHooks' with logstash messages.
+Message::Passing::Output::WebHooks - call 'WebHooks' with messages.
 
 =head1 SYNOPSIS
 
-    logstash --input STDIN --output WebHooks
+    message-pass --input STDIN --output WebHooks
 
     You type:
     {"url": "http://localhost:5000/test","@type":"WebHooks","data":{"foo":"bar"}}
@@ -158,7 +158,7 @@ L<Log::Dispatch> framework.
 
 If you're not already an L<AnyEvent> perl app (most people!), then you can use
 L<Message::Passing::Input::STDIN>, L<Message::Passing::Input::ZeroMQ>
-or any other input class, and the command line logstash utility supplied to run a worker
+or any other input class, and the command line message-pass utility supplied to run a worker
 process, then send messages to it.
 
 To send messages, you can either use Java or Ruby logstash L<http://logstash.net/>, or
